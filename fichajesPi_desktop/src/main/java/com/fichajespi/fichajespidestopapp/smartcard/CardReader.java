cca92557dfff70f1179861743fe26d88da083cd4
Alejandro Ferrín <aferrin.dev@gmail.com>
2021-12-10 16:45:26 +0100

    Added mouse click simulation to desktop app in order to wake up the screen when card read

--- fichajesPi_desktop/src/main/java/com/fichajespi/fichajespidestopapp/smartcard/CardReader.java
+++ fichajesPi_desktop/src/main/java/com/fichajespi/fichajespidestopapp/smartcard/CardReader.java
@@ -8,6 +8,8 @@ package com.fichajespi.fichajespidestopapp.smartcard;
 import com.fichajespi.fichajespidestopapp.MainWindow;
 import com.fichajespi.fichajespidestopapp.entity.Fichaje;
 import com.fichajespi.fichajespidestopapp.httpClient.RequestSender;
+import java.awt.Robot;
+import java.awt.event.InputEvent;
 
 import java.io.IOException;
 import java.math.BigInteger;
@@ -27,91 +29,98 @@ import javax.smartcardio.TerminalFactory;
  */
 public class CardReader extends Thread {
 
-	private MainWindow instance;
-	private RequestSender rs;
+  private MainWindow instance;
+  private RequestSender rs;
 
-	public CardReader(MainWindow instance) {
-		this.instance = instance;
-		this.rs=new RequestSender();
-	}
+  public CardReader(MainWindow instance) {
+    this.instance = instance;
+    this.rs = new RequestSender();
+  }
 
-	@Override
-	public void run() {
-		while (true) {
+  @Override
+  public void run() {
+    while (true) {
 
-			try {
-				// Display the list of terminals
-				TerminalFactory factory = TerminalFactory.getDefault();
-				List<CardTerminal> terminals = factory.terminals().list();
-				//System.out.println("Terminals: " + terminals);
+      try {
+        // Display the list of terminals
+        TerminalFactory factory = TerminalFactory.getDefault();
+        List<CardTerminal> terminals = factory.terminals().list();
+        //System.out.println("Terminals: " + terminals);
 
-				// Use the first terminal
-				CardTerminal terminal = terminals.get(0);
+        // Use the first terminal
+        CardTerminal terminal = terminals.get(0);
 
-				// Connect wit hthe card
-				if (terminal.isCardPresent()) {
-					Card card = terminal.connect("*");
-					//System.out.println("Card: " + card);
-					CardChannel channel = card.getBasicChannel();
+        // Connect wit hthe card
+        if (terminal.isCardPresent()) {
 
-					// Send test command
-					ResponseAPDU response = channel.transmit(new CommandAPDU(new byte[]{
-						(byte) 0xFF,
-						(byte) 0xCA,
-						(byte) 0x00,
-						(byte) 0x00,
-						(byte) 0x00}));
-					//System.out.println("Response: " + response.toString());
+          
+          //Simulamos un click de ratón para despertar la pantalla si se ha apagado
+          Robot robot = new Robot();
+          robot.mousePress(InputEvent.BUTTON1_MASK);
+          robot.mouseRelease(InputEvent.BUTTON1_MASK);
 
-					if (response.getSW1() == 0x63 && response.getSW2() == 0x00) {
-						//System.out.println("Failed");
-					} else {
-						//System.out.println("UID: " + bin2hex(response.getData()));
-						BigInteger decimal = new BigInteger(bin2hex(response.getData()), 16);
-						System.out.println(decimal);
-						fichar(decimal.toString());
-						// Disconnect the card
-						card.disconnect(false);
-					}
+          Card card = terminal.connect("*");
+          //System.out.println("Card: " + card);
+          CardChannel channel = card.getBasicChannel();
 
-				}
+          // Send test command
+          ResponseAPDU response = channel.transmit(new CommandAPDU(new byte[]{
+            (byte) 0xFF,
+            (byte) 0xCA,
+            (byte) 0x00,
+            (byte) 0x00,
+            (byte) 0x00}));
+          //System.out.println("Response: " + response.toString());
 
-			} catch (Exception e) {
-				//System.out.println("Ouch: " + e.toString());
+          if (response.getSW1() == 0x63 && response.getSW2() == 0x00) {
+            //System.out.println("Failed");
+          } else {
+            //System.out.println("UID: " + bin2hex(response.getData()));
+            BigInteger decimal = new BigInteger(bin2hex(response.getData()), 16);
+            System.out.println(decimal);
+            fichar(decimal.toString());
+            // Disconnect the card
+            card.disconnect(false);
+          }
 
-			}
-		}
-	}
+        }
 
-	private void fichar(String number) throws InterruptedException, IOException {
-		Fichaje fichaje = rs.sendRequest(number);
-		if (fichaje != null) {
-			System.out.println("Fichaje OK");
-			instance.changeNumero(number);
-			instance.changeNombre(fichaje.getNombreUsuario());
-			StringBuilder builder = new StringBuilder();
-			builder.append("Hora de ");
-			builder.append(fichaje.getTipo());
-			builder.append(": ");
-			//DateTimeFormatter formatterIn = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
-			//LocalDateTime dateTime = LocalDateTime.parse(fichaje.getTime(), formatterIn);
-			LocalTime dateTime = LocalTime.parse(fichaje.getHora());
-			DateTimeFormatter formatterOut = DateTimeFormatter.ofPattern("HH:mm:ss");
-			builder.append(dateTime.format(formatterOut));
-			instance.changeFichaje(builder.toString());
+      } catch (Exception e) {
+        //System.out.println("Ouch: " + e.toString());
 
-			CardReader.sleep(3000);
-		} else {
-			instance.changeNumero(number + " no existe");
-			CardReader.sleep(5000);
+      }
+    }
+  }
 
-		}
+  private void fichar(String number) throws InterruptedException, IOException {
+    Fichaje fichaje = rs.sendRequest(number);
+    if (fichaje != null) {
+      System.out.println("Fichaje OK");
+      instance.changeNumero(number);
+      instance.changeNombre(fichaje.getNombreUsuario());
+      StringBuilder builder = new StringBuilder();
+      builder.append("Hora de ");
+      builder.append(fichaje.getTipo());
+      builder.append(": ");
+      //DateTimeFormatter formatterIn = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+      //LocalDateTime dateTime = LocalDateTime.parse(fichaje.getTime(), formatterIn);
+      LocalTime dateTime = LocalTime.parse(fichaje.getHora());
+      DateTimeFormatter formatterOut = DateTimeFormatter.ofPattern("HH:mm:ss");
+      builder.append(dateTime.format(formatterOut));
+      instance.changeFichaje(builder.toString());
 
-		instance.resetScreen();
-	}
+      CardReader.sleep(3000);
+    } else {
+      instance.changeNumero(number + " no existe");
+      CardReader.sleep(5000);
 
-	private String bin2hex(byte[] data) {
-		return String.format("%0" + (data.length * 2) + "X", new BigInteger(1, data));
-	}
+    }
+
+    instance.resetScreen();
+  }
+
+  private String bin2hex(byte[] data) {
+    return String.format("%0" + (data.length * 2) + "X", new BigInteger(1, data));
+  }
 
 }
